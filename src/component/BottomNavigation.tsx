7bb59f228cd40026f7cb6f7bcf913d707aa813d6
이한승 <itstrap1016@gmail.com>
2025-06-19 09:35:44 +0900

    style/코드 수정

--- src/component/BottomNavigation.tsx
+++ src/component/BottomNavigation.tsx
@@ -8,16 +8,15 @@ interface IList {
   svg: string;
   text: string;
   on: boolean;
+  to: string;
 }
 
-function List({ svg, text, on }: IList) {
+function List({ svg, text, on, to }: IList) {
   return (
     <li className="flex flex-col items-center w-21">
-      <Link to="/" className="flex flex-col items-center">
+      <Link to={to} className="flex flex-col items-center">
         <img src={svg} alt={text} className="w-6 h-6" />
-        <span
-          className={`text-xs mt-1 ${on ? 'text-[#FF7E36]' : 'text-[#767676]'}`}
-        >
+        <span className={`text-xs mt-1 ${on ? 'text-main' : 'text-gray'}`}>
           {text}
         </span>
       </Link>
@@ -28,10 +27,10 @@ function List({ svg, text, on }: IList) {
 function BottomNavigation() {
   return (
     <ul className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 flex justify-around items-center py-2 px-[6px] z-50 h-15">
-      <List svg={iconHomeFill} text={'홈'} on={true} />
-      <List svg={iconMessageCircle} text={'채팅'} on={false} />
-      <List svg={iconEdit} text={'게시물 작성'} on={false} />
-      <List svg={iconUser} text={'프로필'} on={false} />
+      <List svg={iconHomeFill} text={'홈'} on={true} to={'/'} />
+      <List svg={iconMessageCircle} text={'채팅'} on={false} to={'/chat'} />
+      <List svg={iconEdit} text={'게시물 작성'} on={false} to={'/write'} />
+      <List svg={iconUser} text={'프로필'} on={false} to={'/profile'} />
     </ul>
   );
 }
