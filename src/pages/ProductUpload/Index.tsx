a8b853c6779c232635e31f4b76831e1642931e20
이한승 <itstrap1016@gmail.com>
2025-06-30 17:41:48 +0900

    feat/product-upload 경로 추가

---
+++ src/pages/ProductUpload/Index.tsx
@@ -0,0 +1,11 @@
+import TopNavigation from '../../component/Navigation/TopNavigation';
+
+function ProductUpload() {
+  return (
+    <>
+      <TopNavigation backBtn={true} />
+    </>
+  );
+}
+
+export default ProductUpload;
