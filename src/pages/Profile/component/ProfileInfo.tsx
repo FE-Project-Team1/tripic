71369555547c776379e514ae0785af769a0e4afc
나연규 <yeongyu1112@gmail.com>
2025-07-01 11:18:21 +0900

    받은 데이터를 화면에 렌더링 되도록하였습니다

--- src/pages/Profile/component/ProfileInfo.tsx
+++ src/pages/Profile/component/ProfileInfo.tsx
@@ -26,30 +26,40 @@ function ProfileInfo() {
   // 에러 발생 시
   if (isError) return <div>프로필 정보를 불러오는 중 오류가 발생했습니다.</div>;
 
+  // 프로필 데이터
+  const profile = data?.profile;
+
   return (
     <section className="flex flex-col items-center max-w-[390px] h mx-auto overflow-auto">
       <h2 className="sr-only">프로필 정보</h2>
       <div className="w-full flex justify-between items-center mt-[30px] mb-[16px]">
         <div className="flex flex-col items-center ml-[41px]">
-          <span className="text-lg font-bold">2950</span>
+          <span className="text-lg font-bold">
+            {profile?.followerCount || 0}
+          </span>
           <span className="text-[10px] text-gray">followers</span>
         </div>
         {/* 프로필 이미지 영역 (빈 공간으로 유지) */}
         <div className="w-[110px] h-[110px] flex items-center justify-center">
           <ProfileImage />
         </div>
-        {/* 프로필 이미지 영역 (빈 공간으로 유지) */}
         <div className="flex flex-col items-center mr-[45px]">
-          <span className="text-lg font-bold text-gray">128</span>
+          <span className="text-lg font-bold text-gray">
+            {profile?.followingCount || 0}
+          </span>
           <span className="text-[10px] text-gray">followings</span>
         </div>
       </div>
 
       <div className="flex flex-col items-center text-center w-full">
-        <p className="font-bold text-base mb-[6px]">애월읍 위니브 감귤농장</p>
-        <p className="text-xs text-gray">@ weniv_Mandarin</p>
+        <p className="font-bold text-base mb-[6px]">
+          {profile?.username || '사용자'}
+        </p>
+        <p className="text-xs text-gray">
+          @ {profile?.accountname || 'accountname'}
+        </p>
         <p className="text-sm text-gray mt-[16px] mb-[24px]">
-          애월을 감귤 전국 배송, 곶감키 제철, 감귤 농장
+          {profile?.intro || '소개글이 없습니다'}
         </p>
       </div>
       <div className="flex flex-row items-center justify-center w-full mb-[26px] gap-[10px]">
@@ -57,7 +67,13 @@ function ProfileInfo() {
           <img src={messageBtn} alt="메시지" />
         </button>
         <div className="w-30">
-          <CommonBtn text="팔로우" size="medium" />
+          {profile?.isfollow ? (
+            <button className="w-[120px] h-[34px] bg-[#FFFFFF] border border-light-gray rounded-[30px] text-gray text-[14px] font-medium flex items-center justify-center">
+              언팔로우
+            </button>
+          ) : (
+            <CommonBtn text="팔로우" size="medium" />
+          )}
         </div>
         <button>
           <img src={shareBtn} alt="공유하기" />
