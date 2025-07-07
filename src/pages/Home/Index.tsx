e2cf8d6261cfa8bb245537c0298d9195d491001c
이한승 <itstrap1016@gmail.com>
2025-07-07 11:46:20 +0900

    feat/home-feed test

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,12 +1,24 @@
+import { useQuery } from '@tanstack/react-query';
+import { getPostFeed } from '../../api/post/getPostFeed';
 import TopNavigation from '../../component/Navigation/TopNavigation';
 import BottomNavigation from '../../component/BottomNavigation';
 import DefaultHome from './component/DefaultHome';
 
 function Home() {
+  const { data } = useQuery({
+    queryKey: ['postFeed'],
+    queryFn: () => getPostFeed(10, 0), // 테스트용 10개만
+    enabled: true, // 자동 실행
+    staleTime: 0, // 캐시 사용 안함 (테스트용)
+    retry: 1,
+  });
+
+  console.log(data, data?.posts?.length);
+
   return (
     <>
       <TopNavigation heading="Tripic" searchIcon={true} />
-      <DefaultHome />
+      {data?.posts && data?.posts.length > 0 ? <>test</> : <DefaultHome />}
       <BottomNavigation activePage="Home" />
     </>
   );
