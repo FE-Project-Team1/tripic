e54fdfe263b9167f932b00ea217a1ba96200a03d
이한승 <itstrap1016@gmail.com>
2025-07-09 10:47:53 +0900

    feat/Home Bottom Modal 추가

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,6 +1,7 @@
 import TopNavigation from '../../component/Navigation/TopNavigation';
 import BottomNavigation from '../../component/BottomNavigation';
 import Feeds from './component/Feeds';
+import BottomModal from '../../component/BottomModal';
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <TopNavigation heading="Tripic" searchIcon={true} />
       <Feeds />
       <BottomNavigation activePage="Home" />
+      <BottomModal />
     </>
   );
 }
