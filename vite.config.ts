5899407ec1abe2458fba1fce67d304188fdfacf0
이한승 <itstrap1016@gmail.com>
2025-07-09 10:43:42 +0900

    feat/배포 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import removeConsole from 'vite-plugin-remove-console';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), removeConsole()],
   base: '/tripic/', // 레포지토리 이름으로 수정
   build: {
     outDir: 'dist',
